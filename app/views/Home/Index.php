69a938a008e191e90bff8a0b4ddcbdfdffa62e53
cmmorton <colinmorton8@gmail.com>
2019-12-18 11:02:50 -0600

    beginning home page

--- app/views/Home/Index.php
+++ app/views/Home/Index.php
@@ -9,6 +9,27 @@
 	<?php View::inc("header"); ?>
 </head>
 <body>
+	<div class="container">
+		<div class="row">
+			<h1 class="col-md-6">Harris County Professional Gasses</h1>
+			<ul>
+				<h4>These are some ammenadies</h4>
+				<br>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+			</ul>
+		</div>
+	</div>
+
+	<h2 class= "Text Center">Sample text for creating your account below</h2>
+	
+	<div class="container">
+		<div class="row">
+			<div class="jumbotron"></div>
+		</div>
+	</div>
+	
 	<?php View::inc("footer"); ?>
 </body>
 </html>
