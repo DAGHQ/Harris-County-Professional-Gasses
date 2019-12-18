f7d0c7134a817b347bd9e75f801d4da03c54622e
cmmorton <colinmorton8@gmail.com>
2019-12-18 14:43:36 -0600

    Update Index.php

--- app/views/Home/Index.php
+++ app/views/Home/Index.php
@@ -65,10 +65,9 @@
 	</div>
 	<br>
 	
-	/* I want to allign these cards centered on the page but i dont know how to do that. */
-	
 	<div class="container">
 		<div class="row">
+			<div class="col-md-1"></div>
 			<div class="card col-md-3">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi soluta perferendis officia labore odio minus, unde totam, delectus corporis itaque tenetur ipsa optio officiis culpa debitis dolore molestiae minima eius.</p>
 			</div>
@@ -80,6 +79,7 @@
 			<div class="card col-md-3">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste voluptatem at architecto quidem iure inventore nobis officiis illum totam adipisci laudantium porro nesciunt fugit doloremque culpa, quod praesentium, quisquam debitis.</p>
 			</div>
+			<div class="col-md-2"></div>
 		</div>
 	</div>
 	<?php View::inc("footer"); ?>
