62c29a9c720bdc58ba30d6d1976692808eb14ecf
DAGWebs <38997647+DAGWebs@users.noreply.github.com>
2019-12-21 14:26:39 -0600

    fixed links

--- app/views/inc/navbar-top.php
+++ app/views/inc/navbar-top.php
@@ -3,8 +3,8 @@
 			<ul>
 				<li><a href="/home">Home</a></li>
 				<li><a href="/about">About</a></li>
-				<li><a href="/Support">Support</a></li>
-				<li><a href="/Contact">Contact Us</a></li>
+				<li><a href="/support">Support</a></li>
+				<li><a href="/contact">Contact Us</a></li>
 				<li><a href="/login">Login</a></li>
 				<li><a href="/register">Register</a></li>
 			</ul>
